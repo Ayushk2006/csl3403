4094cd8cf57b56c3666f7f9be7efae9aa91a0684
Ayushk2006 <90329143+Ayushk2006@users.noreply.github.com>
2021-11-27 22:46:06 +0530

    Create object.js

---
+++ object.js
@@ -0,0 +1,14 @@
+class Person {
+  constructor(firstName, lastName, officeAdress) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.officeAdress = officeAdress;
+  }
+}
+var person1 = new Person("Sonu", "Aarya", "patel chowk,bhabua");
+var person2 = new Person("Abhishek", "Kumar", "Ekta chowk,bhabua");
+var person3 = new Person("Aayush", "Sharma", "Nitp,patna");
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
